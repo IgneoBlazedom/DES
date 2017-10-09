238c1a9d693d5ff8759e598d4c6b08d0820e7229
Hot Cakes <Hot Cakes@yoyotiddies>
2017-10-08 20:18:20 -0500

    Merge origin/master
    Conflicts:
    	web/alta.jsp

--- Keys/src/Dess/LeftRights.java
+++ Keys/src/Dess/LeftRights.java
@@ -6,68 +6,81 @@ import java.util.Scanner;
 public class LeftRights {
     
     public void ObtenerLR(){
+        
         Scanner leer = new Scanner(System.in);
         Convertir Convertir = new Convertir();
-        String dato="";
+        String dato;
         System.out.println("Ingrese el Dato en Hexadecimal:");
         dato = (Convertir.convertido(leer.next()));
         System.out.println("Tu Dato en binario es: "+dato);
         
-        int[] cIP = {58, 50, 42, 34, 26, 18, 10, 2, 
-                    60, 52, 44, 36, 28, 20, 12, 4,
-                    62, 54, 46, 38, 30, 22, 14, 6,
-                    64, 56, 48, 40, 32, 24, 16, 8,
-                    57, 49, 41, 33, 25, 17, 9, 1,
-                    59, 51, 43, 35, 27, 19, 11, 3,
-                    61, 53, 45, 37, 29, 21, 13, 5,
-                    63, 55, 47, 39, 31, 23, 15, 7};
-        
-        String dIP="";
-        
-        for (int i = 0; i<64; i++) {            
-            dIP = (dIP+dato.charAt(cIP[i]-1));
-        }
-        System.out.println(dIP);
-        //Hasta aquí ya está permutado
-        
         String left0 = "";
         String right0 = "";
-        for (int i = 0; i < 64; i++) {
-            if (i<32) 
-                left0 = (left0+dIP.charAt(i));            
-            else
-                right0 = (right0+dIP.charAt(i));
-        }
+        String dIP="";
+//        Convocamos LLaves
+        ArrayList<String> Llaves;                    
+        Keys Keys = new Keys();
+        Llaves = Keys.obtenerLlaves();
+            
+            int[] cIP = {58, 50, 42, 34, 26, 18, 10, 2, 
+                        60, 52, 44, 36, 28, 20, 12, 4,
+                        62, 54, 46, 38, 30, 22, 14, 6,
+                        64, 56, 48, 40, 32, 24, 16, 8,
+                        57, 49, 41, 33, 25, 17, 9, 1,
+                        59, 51, 43, 35, 27, 19, 11, 3,
+                        61, 53, 45, 37, 29, 21, 13, 5,
+                        63, 55, 47, 39, 31, 23, 15, 7};
+
         
-        System.out.println("L = "+left0);
-        System.out.println("R = "+right0);
-        //Left&Right ya divididos
         
-        String left ="";
-        String right="";
-        String prodxor="";
-        int cExp[] = {32, 1, 2, 3, 4, 5,
-                      4, 5, 6, 7, 8, 9,
-                      8, 9, 10, 11, 12, 13, 
-                      12, 13, 14, 15, 16, 17,
-                      16, 17, 18, 19, 20, 21,
-                      20, 21, 22, 23, 24, 25,
-                      24, 25, 26, 27, 28, 29, 
-                      28, 29, 30, 31, 32, 1};
+            for (int j = 0; j<64; j++) {            
+                dIP = (dIP+dato.charAt(cIP[j]-1));
+            }
+            System.out.println("PermIP:"+dIP);
+        //Hasta aquí ya está permutado
+        
+        
+        
+            for (int j = 0; j < 64; j++) {
+                if (j<32) 
+                    left0 = (left0+dIP.charAt(j));            
+                else
+                    right0 = (right0+dIP.charAt(j));
+            }
+            
+            for (int i = 0; i < 16; i++) {
+            System.out.println("Nuevo***************************************************************************");
+            System.out.println("L"+(i)+" = "+left0);
+            System.out.println("R"+(i)+" = "+right0);
+            //Left&Right ya divididos
+
+            String right="";
+            String prodxor="";
+            int cExp[] = {32, 1, 2, 3, 4, 5,
+                          4, 5, 6, 7, 8, 9,
+                          8, 9, 10, 11, 12, 13, 
+                          12, 13, 14, 15, 16, 17,
+                          16, 17, 18, 19, 20, 21,
+                          20, 21, 22, 23, 24, 25,
+                          24, 25, 26, 27, 28, 29, 
+                          28, 29, 30, 31, 32, 1};
+         //Convocamos LLaves
+//            ArrayList<String> Llaves;                    
+//            Keys Keys = new Keys();
+//            Llaves = Keys.obtenerLlaves();
+//            
 //        for (int i = 1; i <= 16; i++) {
+//            System.out.println("Nuevo***************************************************************************");
             for (int j = 0; j < cExp.length; j++) 
             right = (right+right0.charAt(cExp[j]-1));
             
-            //Convocamos LLaves
-            ArrayList<String> Llaves;                    
-            Keys Keys = new Keys();
-            Llaves = Keys.obtenerLlaves();
-            for (int j = 0; j < 1; j++) {
+           
+            for (int j = 0; j<1; j++) {
                 System.out.println("");
-                System.out.println("R"+j+" = "+right);
-                System.out.println("K"+(j+1)+" = "+Llaves.get(j));
-                for (int k = 0; k < Llaves.get(j).length(); k++) {
-                    if (right.charAt(k)==Llaves.get(j).charAt(k))
+                System.out.println("R"+i+" = "+right);
+                System.out.println("K"+(i+1)+" = "+Llaves.get(i));
+                for (int k = 0; k < Llaves.get(i).length(); k++) {
+                    if (right.charAt(k)==Llaves.get(i).charAt(k))
                         prodxor+="0";
                     else
                         prodxor+="1";
@@ -148,25 +161,39 @@ public class LeftRights {
                     Convertir Conv = new Convertir();
                     afterbox = afterbox+Conv.pacajas(coords[k]);                    
                 }
-                System.out.println(afterbox);
+                System.out.println("Después de S's"+afterbox);
                 //Hasta aquí saca las coordenadas
                 
                 int perm[] = {16, 7, 20, 21,29, 12, 28, 17,1, 15, 23, 26,5, 18, 
                   31, 10,2, 8, 24, 14,32, 27, 3, 9,19, 13, 30, 6,22, 11, 4, 25};
                 
-                String permp;
-                for (int i = 0; i < afterbox.length(); i++) {
-                    
+                String permp="";
+                for (int k = 0; k < afterbox.length(); k++) {
+                    permp = (permp+afterbox.charAt(perm[k]-1));
                 }
-                
-                
-                
-                
-                
+                String xorLnP="";
+                for (int k = 0; k < permp.length(); k++) {
+                    if (permp.charAt(k)==left0.charAt(k)) 
+                        xorLnP=xorLnP+"0";
+                    else
+                       xorLnP=xorLnP+"1"; 
+                }
+                System.out.println("");
+                System.out.println(permp+"  Permutada con P");
+                System.out.println(left0+"  Left"+i);
+                System.out.println("------------------------------XoR----------------------");
+                System.out.println(xorLnP);
+                System.out.println("");
+                left0=right0;
+                right0=xorLnP;
+                System.out.println("L"+(i+1)+" = "+left0);
+                System.out.println("R"+(i+1)+" = "+right0);
+                right = "";
+                prodxor="";
             }
             
             
-//        }
+        }
      
         
         
